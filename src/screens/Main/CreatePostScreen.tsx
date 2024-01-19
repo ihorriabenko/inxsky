35e37dddd94a3ab45425dba6fad281df770fb1da
Ihor Riabenko <100082550+ihorriabenko@users.noreply.github.com>
2024-01-19 18:51:55 +0200

    Chore: Tried out the post screen

--- src/screens/Main/CreatePostScreen.tsx
+++ src/screens/Main/CreatePostScreen.tsx
@@ -1,9 +1,146 @@
-import { Text, View } from "react-native"
+import { Camera, CameraType } from 'expo-camera';
+import * as Location from 'expo-location';
+import { useState } from 'react';
+import {
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import {
+  BottomTabScreenProps,
+  BottomTabParamList,
+} from '../../navigation/navigation.type';
+import { LocationObject } from 'expo-location';
 
-export const CreatePostScreen = () => {
+type Props = BottomTabScreenProps<BottomTabParamList, 'CreatePost'>;
+export const CreatePostScreen = (props: Props) => {
+  const { navigation } = props;
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [cameraRef, setCameraRef] = useState<Camera | null>(null);
+  const [location, setLocation] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
+  const [photoUri, setPhotoUri] = useState<string | undefined>();
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={s.container}>
+        <Text style={{ textAlign: 'center' }}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  const toggleCameraType = () => {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
+  };
+
+  const takePhoto = async () => {
+    const photo = await cameraRef?.takePictureAsync();
+    setPhotoUri(photo?.uri);
+  };
+
+  const shareLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+
+    if (status !== 'granted') {
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+
+    setLocation((prev) => ({
+      ...prev,
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+    }));
+  };
+
   return (
-    <View>
-      <Text>CreatePostScreen</Text>
+    <View style={s.container}>
+      <Camera style={s.camera} type={type} ref={setCameraRef}>
+        {/* Image */}
+        <View style={{ marginTop: 100 }}>
+          <Image
+            source={{ uri: photoUri }}
+            style={{ width: 100, height: 100 }}
+          />
+          <TouchableOpacity
+            style={{ width: 50, height: 20, backgroundColor: 'red' }}
+            onPress={() =>
+              navigation.navigate('ProfileNativeStack', {
+                screen: 'Profile',
+                params: {
+                  photoUri,
+                  ...location,
+                },
+              })
+            }
+          >
+            <Text style={{ textAlign: 'center' }}>Send</Text>
+          </TouchableOpacity>
+        </View>
+        {/* Buttons */}
+        <TouchableOpacity
+          style={{ height: 50, width: 50, backgroundColor: 'red' }}
+          onPress={shareLocation}
+        >
+          <Text>Share location</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={s.buttonToggle} onPress={toggleCameraType}>
+          <AntDesign name="sync" size={26} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity style={s.button} onPress={takePhoto} />
+      </Camera>
     </View>
-  )
-}
+  );
+};
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+  button: {
+    position: 'absolute',
+    bottom: 10,
+    alignSelf: 'center',
+
+    width: 70,
+    height: 70,
+    borderRadius: 99,
+    borderWidth: 3,
+    borderColor: '#fff',
+
+    backgroundColor: 'grey',
+    opacity: 0.5,
+  },
+  buttonToggle: {
+    position: 'absolute',
+    bottom: 33,
+    right: 55,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
--- src/screens/Main/ProfileScreen.tsx
+++ src/screens/Main/ProfileScreen.tsx
@@ -1,9 +1,40 @@
-import { Text, View } from "react-native"
+import { useEffect, useState } from 'react';
+import { Button, FlatList, Image, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { ProfileScreenProps } from '../../navigation/navigation.type';
+import { auth } from '../../firebase/firebase.config';
+import { useAppDispatch } from '../../redux/redux.hook';
+import { signOut } from '../../redux/auth/auth.action';
 
-export const ProfileScreen = () => {
+export const ProfileScreen = (props: ProfileScreenProps) => {
+  const { route, navigation } = props;
+  const [posts, setPosts] = useState<any[]>([]);
+  const insets = useSafeAreaInsets();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (route.params) setPosts((prevState) => [...prevState, route.params]);
+  }, [route.params]);
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View style={{ paddingTop: insets.top, paddingHorizontal: 16 }}>
+      <FlatList
+        data={posts}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => (
+          <View style={{ marginBottom: 8 }}>
+            <Image
+              source={{ uri: item.photo }}
+              style={{ height: 100, width: '100%' }}
+            />
+          </View>
+        )}
+      />
+      <Button
+        title="go to map"
+        onPress={() => navigation.navigate('PostStack', { screen: 'Map' })}
+      />
+      <Button title="sign out" onPress={() => dispatch(signOut())} />
     </View>
-  )
-}
+  );
+};
